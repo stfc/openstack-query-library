676465c53bacda4e8729bb87879105c0fba2dcfc
David Fairbrother <DavidFair@users.noreply.github.com>
2025-01-03 12:19:45 +0000

    Drop resources from HV props which return none
    These resources all return None for HVs, as Yoga+ expects us to use a
    resource object instead.
    
    Remove these since they will not work again, and any downstream usage
    will fail loudly when they try to import the dead enum.
    This will require a major version bump which will come in the next
    commit adding the placement replacement.

--- tests/runners/test_hypervisor_runner.py
+++ tests/runners/test_hypervisor_runner.py
@@ -24,11 +24,9 @@ def test_parse_query_params(instance):
     )
 
 
-@patch("openstackquery.runners.hypervisor_runner.json.loads")
 @patch("openstackquery.runners.runner_utils.RunnerUtils.run_paginated_query")
 def test_run_query_no_server_filters(
     mock_run_paginated_query,
-    mock_json_loads,
     instance,
     mock_marker_prop_func,
 ):
@@ -49,26 +47,6 @@ def test_run_query_no_server_filters(
 
     mock_connection = MagicMock()
 
-    vcpu_resource_class = MagicMock()
-    vcpu_resource_class.resource_class = "VCPU"
-    vcpu_resource_class.total = 128
-    memory_resource_class = MagicMock()
-    memory_resource_class.resource_class = "MEMORY_MB"
-    memory_resource_class.total = 515264
-    disk_resource_class = MagicMock()
-    disk_resource_class.resource_class = "DISK_GB"
-    disk_resource_class.total = 3510
-
-    mock_connection.placement.resource_provider_inventories.return_value = (
-        vcpu_resource_class,
-        memory_resource_class,
-        disk_resource_class,
-    )
-
-    mock_json_loads.return_value = {
-        "usages": {"VCPU": 4, "MEMORY_MB": 8192, "DISK_GB": 10}
-    }
-
     res = instance.run_query(
         mock_connection,
         filter_kwargs=None,
@@ -80,17 +58,4 @@ def test_run_query_no_server_filters(
         {"details": True},
     )
 
-    assert mock_json_loads.call_count == 2
-
     assert res == mock_hv_list
-
-    assert mock_hv1.resources == {
-        "VCPU": {"total": 128, "usage": 4, "free": 124},
-        "MEMORY_MB": {"total": 515264, "usage": 8192, "free": 507072},
-        "DISK_GB": {"total": 3510, "usage": 10, "free": 3500},
-    }
-    assert mock_hv2.resources == {
-        "VCPU": {"total": 128, "usage": 4, "free": 124},
-        "MEMORY_MB": {"total": 515264, "usage": 8192, "free": 507072},
-        "DISK_GB": {"total": 3510, "usage": 10, "free": 3500},
-    }
